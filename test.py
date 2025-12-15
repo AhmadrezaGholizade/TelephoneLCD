ed947f9acc84c9255157d8c8a1b25f39523db686
AhmadrezaGholizade <ahmadrezagholizade304@gmail.com>
2025-12-15 16:59:42 +0330

    main page sketch completed.

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 from PIL import Image
 from LCD.FB import FrameBuffer
 
-if __name__ == "__main__":
-    fb = FrameBuffer()
-    
-    # Load and resize image manually
-    img = Image.open("test.png")
-    img = img.resize((fb.width, fb.height), Image.LANCZOS)
-    fb.write(img)
-    
-    # Or let write() handle resizing automatically
-    img = Image.open("test.png")
-    fb.write(img)
+fb = FrameBuffer()
+
+# Load and resize image manually
+img = Image.open("test.png")
+img = img.resize((fb.width, fb.height), Image.LANCZOS)
+fb.write(img)
+
+# Or let write() handle resizing automatically
+img = Image.open("test.png")
+fb.write(img)
