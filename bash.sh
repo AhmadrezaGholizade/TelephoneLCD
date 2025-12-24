4e1eedb43dfd84420ebcff21361fe3b82306bc6b
AhmadrezaGholizade <ahmadrezagholizade304@gmail.com>
2025-12-24 11:30:01 +0330

    requirements added

--- bash.sh
+++ bash.sh
@@ -19,3 +19,11 @@ echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bas
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 source ~/.bashrc
+
+# Requirements
+# jalali_core 1.0.0
+# jdatetime   5.2.0
+# numpy       2.2.6
+# pillow      12.0.0
+# pip         23.0.1
+# setuptools  65.5.0
